9711601fac114c17f3f1faa9168ca01cfe239a3f
wiuver <wiuver.ribeiro@gmail.com>
2021-02-05 13:57:01 -0300

    Finalizando CRUD de usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,50 @@
+// const { update } = require('../database');
 const knex = require('../database');
+// const { delete } = require('../routes');
 
 module.exports = {
   async index(request, response) {
     const result = await knex('users');
 
     return response.json(result);
-  }
+  },
+
+  async create(request, response, next) {
+    try {
+
+      const { username } = request.body;
+      await knex('users').insert({
+        username
+      });
+      return response.status(201).send('');
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  async update(request, response, next) {
+    try {
+      const { id } = request.params;
+      const { username } = request.body;
+
+      await knex('users').update({
+        username
+      }).where({ id });
+      return response.status(201).send('');
+
+    } catch (error) {
+      next(error);  
+    }
+  },
+
+  async delete(request, response, next) {
+    try {
+      const { id } = request.params;
+      await knex('users').where({ id }).delete();
+
+      return response.send('');
+    } catch (error) {
+      next(error);
+    }
+  },
 } 
--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 const routes = require('./routes');
-
+app.use(express.json());
 app.use(routes);
 
+app.use((error, request, response, next) => {
+  response.status(error.status || 500);
+  response.json({error:error.message})
+})
 
-app.listen(3333, () => console.log('Server is running at '));
+app.listen(3333, () => console.log('Server is running'));
